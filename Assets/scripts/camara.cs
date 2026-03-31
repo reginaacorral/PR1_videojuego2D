75c12b50325fcec6ae7c43acc04e9c2ae4d347db
reginaacorral <rcordel@upv.edu.es>
2026-03-31 14:24:19 +0200

    avance

---
+++ Assets/scripts/camara.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class camara : MonoBehaviour
+{
+    public GameObject persdonaje;
+    Vector3 dondePersonaje;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+dondePersonaje = 
+    }
+}
--- Assets/scripts/movpersonaje.cs
+++ Assets/scripts/movpersonaje.cs
@@ -4,42 +4,57 @@ using UnityEngine.InputSystem;
 public class movpersonaje : MonoBehaviour
 {
     public float velocidad = 0.5f;
+    public float impulsoSalto = 1.0f;
 
-    Rigibody2D rb;
+    Rigidbody2D rb;
 
-bool estoySaltando = false;
+bool puedoSaltar = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
 
-        rb = GetComponent<Rigibody2D>();
-    
     }
 
     // Update is called once per frame
     void Update()
     {
-    
         Vector2 moveInput = InputSystem.actions["Move"].ReadValue<Vector2>();
         this.transform.Translate(moveInput.x*velocidad, 0, 0);
-        
-//flip
 
-        if(moveInput.x > 0)
+//flip
+        if(moveInput.x < 0)
         {
-        this.GetComponent<SpriteRenderer>().flipX = false;
+        this.GetComponent<SpriteRenderer>().flipX = true;
+    }else if(moveInput.x > 0)
+{
+    this.GetComponent<SpriteRenderer>().flipX = false;
     }
-    
+
+
+RaycastHit2D hit = Physics2D.Raycast(transform.position,Vector2.down*0.5f);
+Debug.DrawRay(transform.position,Vector2.down*0.5f, Color.red);
+
+if(hit.collider == true)
+    {
+puedoSaltar = true;
+this.GetComponent<SpriteRenderer>().color = Color.red;
+transform.localScale = new Vector3(1,1,1);
+    }
+else
+    {
+puedoSaltar = false;
+this.GetComponent<SpriteRenderer>().color = Color.white;
+transform.localScale = new Vector3(2,2,1);
+     }
 
 
 //salto
     bool salto = InputSystem.actions["Jump"].WasPressedThisFrame();
-    if(salto == true)
+    if(salto == true && puedoSaltar == true)
     {
-Debug.Log("salto");
-     rb.Addforce(transform.up,ForceMode2D.Impulse);
-    
+     rb.AddForce(transform.up*impulsoSalto,ForceMode2D.Impulse);
     }
     
 
